18180155a52d9531efeead40009fff6aa5fb44a0
Aleksei Ivanov <profpopoff@gmail.com>
2023-02-17 11:40:48 +0300

    Fix responsiveness

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import he from 'he' // decoder for &#1337; or smth
 export default function App() {
   // * state that starts the game
   const [start, setStart] = React.useState(false)
-  
+
   // * state that shows the results 
   const [check, setCheck] = React.useState(false)
 
@@ -64,22 +64,25 @@ export default function App() {
 
   // * function that changes the value isClicked of clicked answer from allAnswers to clicked
   function handleAnswerClick(event) {
-    const {name, value} = event.target
+    const { name, value } = event.target
     setQuiz(prevQuiz => prevQuiz.map(quizElement => {
-      return quizElement.id === name ? 
-      {...quizElement,
-        allAnswers: quizElement.allAnswers.map(answer => {
-          return answer.answer === value ?
-          {...answer, 
-            isClicked: !answer.isClicked} :
-          {...answer, isClicked: false}
-        }),
-        isCorrect: quizElement.correctAnswer === value ? true : false
-      } :
-      quizElement
+      return quizElement.id === name ?
+        {
+          ...quizElement,
+          allAnswers: quizElement.allAnswers.map(answer => {
+            return answer.answer === value ?
+              {
+                ...answer,
+                isClicked: !answer.isClicked
+              } :
+              { ...answer, isClicked: false }
+          }),
+          isCorrect: quizElement.correctAnswer === value ? true : false
+        } :
+        quizElement
     }))
   }
-  
+
   // * rendering questions and answers into app
   const quizElements = quiz.map(quizElement => (
     <Question
@@ -95,25 +98,27 @@ export default function App() {
     start ?
       (quiz.length ?
         <main>
-          <div className="questions">{quizElements}</div>
-          <div className="main">
-            {check && <h4 className="main--score">You scored {quiz.filter(quizElement => quizElement.isCorrect).length}/5 correct answers</h4>}
-            <button onClick={() => {checkAnswersOrResetQuiz(); check && getNewQuiz()}} className="main--button default-button">{check ? "Play again" : "Check answers"}</button>
+          <div className="container">
+            <div className="questions">{quizElements}</div>
+            <div className="main">
+              {check && <h4 className="main--score">You scored {quiz.filter(quizElement => quizElement.isCorrect).length}/5 correct answers</h4>}
+              <button onClick={() => { checkAnswersOrResetQuiz(); check && getNewQuiz() }} className="main--button default-button">{check ? "Play again" : "Check answers"}</button>
+            </div>
           </div>
         </main> :
         <main className="loading">
           <div className="spinner-box">
-            <div className="pulse-container"> 
+            <div className="pulse-container">
               <div className="pulse-bubble pulse-bubble-1"></div>
               <div className="pulse-bubble pulse-bubble-2"></div>
               <div className="pulse-bubble pulse-bubble-3"></div>
             </div>
           </div>
         </main>) : (
-      <main className="start">
-        <h1 className="start--title">Quizzical</h1>
-        <h3 className="start--text">Answer 5 questions to get an opportunity to win nothing!</h3>
-        <button onClick={() => {setStart(true); getNewQuiz()}} className="start--button default-button">Start quiz</button>
-      </main>)
+        <main className="start">
+          <h1 className="start--title">Quizzical</h1>
+          <h3 className="start--text">Answer 5 questions to get an opportunity to win nothing!</h3>
+          <button onClick={() => { setStart(true); getNewQuiz() }} className="start--button default-button">Start quiz</button>
+        </main>)
   )
 }
